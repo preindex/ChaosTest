362e9da2d2e9707012709d22089ccea13fd38ab3
preindex <48609126+preindex@users.noreply.github.com>
2024-09-19 17:04:28 -0700

    Initial Commit - Add Generators

---
+++ generators/quantum.js
@@ -0,0 +1,20 @@
+/*
+    quantumRandom
+    Generates a random number given a range using quantum principles.
+*/
+
+export async function quantumRandom(low, high, max) {
+    const URL = 'https://qrandom.io/api/random/ints';
+    const Params = new URLSearchParams({
+        min: low || 1,
+        max: high || 10,
+        n: max || 1
+    });
+    const Response = await fetch(`${URL}?${Params.toString()}`, {
+        method: 'GET'
+    });
+    if (!Response.ok) {
+        throw new Error(`HTTP error! status: ${Response.status} ${Response.statusText}`);
+    }
+    return (await Response.json()).numbers;
+}
