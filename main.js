48df262c10256c25dfe09e9a927f0a2b96878c8f
preindex <48609126+preindex@users.noreply.github.com>
2024-09-26 21:52:17 -0700

    Fix broken test CSV formatting

--- main.js
+++ main.js
@@ -1,73 +1,81 @@
 import {basic} from './generators/basic';
-import { mersenne } from './generators/mersenne';
-import { trueRandom } from './generators/true';
+import {mersenne} from './generators/mersenne';
+import {trueRandom} from './generators/true';
 import {quantumRandom} from './generators/quantum';
-import { logisticMap } from './systems/logisticMap';
-import { randomUUID } from 'crypto';
+import {logisticMap} from './systems/logisticMap';
+import {randomUUID} from 'crypto';
 
 const fs = require('fs');
 
-let Maps = []
+const INITIAL_VALUE = 0.5; // Initial population
+const ITERATIONS = 100; // Number of iterations
+const MAP_COUNT = 15;
+const TRIALS = 1; // Number of trials to conduct.
 
-{ // Linear
-    const x0 = 0.5; // Initial population
-    const iterations = 100; // Number of iterations
-    let Map = []
-    for (var r = 3.5; r < 4; r+=0.034) { // 15x = 3.5
-        Map.push([r, x0, iterations, `"${logisticMap(r, x0, iterations).toString()}`].join(', '))
+const CONSTANT_R = 0.034; // Lets us have 15 different maps in between 3.5 - 4.
+
+async function generateData() {
+    let Maps = ["type, r, initial_value, iterations, map"]
+
+    { // Linear-vanilla logistic maps
+        let Map = []
+        for (let r = 3.5; r < 4; r+= CONSTANT_R) {
+            Map.push(["linear", r, INITIAL_VALUE, ITERATIONS, `"${logisticMap(r, INITIAL_VALUE, ITERATIONS).toString()}"`].join(', '))
+        }
+        console.log("Linear Maps done.")
+        Maps.push(Map.join('\n'))
     }
-    console.log("Linear Maps done.")
-    Maps.push(Map)
+
+    { // Basic-randomized logistic maps
+        let Map = []
+        for (let i = 0; i < 15; i++) {
+            const r = basic(3.5, 4)
+            Map.push(["basic", r, INITIAL_VALUE, ITERATIONS, `"${logisticMap(r, INITIAL_VALUE, ITERATIONS)}"`].join(', '))
+        }
+        console.log("Basic Maps done.")
+        Maps.push(Map.join('\n'))
+    }
+
+    { // mersenne-randomized logistic maps
+        let Map = []
+        for (let i = 0; i < 15; i++) {
+            const r = mersenne(3.5, 4)
+            Map.push(["mersenne", r, INITIAL_VALUE, ITERATIONS, `"${logisticMap(r, INITIAL_VALUE, ITERATIONS)}"`].join(', '))
+        }
+        console.log("Mersenne Maps done.")
+        Maps.push(Map.join('\n'))
+    }
+
+    { // true-randomized logistic maps
+        let Map = []
+        let Numbers = await trueRandom(3.5, 4, 15)
+        for (let i = 0; i < Numbers.length; i++){
+            let r = Numbers[i]
+            console.log(r)
+            Map.push(["true", r, INITIAL_VALUE, ITERATIONS, `"${logisticMap(r, INITIAL_VALUE, ITERATIONS)}"`].join(', '))
+        }
+        console.log("True Maps done.")
+        Maps.push(Map.join('\n'))
+    }
+
+    { // quantum-randomized logistic maps
+        let Map = []
+        let Numbers = await quantumRandom(3.5, 4, 15)
+        for (let i = 0; i < Numbers.length; i++){
+            let r = Numbers[i]
+            Map.push(["quantum", r, INITIAL_VALUE, ITERATIONS, `"${logisticMap(r, INITIAL_VALUE, ITERATIONS)}"`].join(', '))
+        }
+        console.log("Quantum Maps done.")
+        Maps.push(Map.join('\n'))
+    }
+
+    fs.writeFileSync(`data/${randomUUID()}.txt`, Maps.join("\n"));
 }
 
-{ // Basic
-    const x0 = 0.5; // Initial population
-    const iterations = 100; // Number of iterations
-    let Map = []
-    for (var i = 0; i < 15; i++) {
-        const r = basic(3.5, 4)
-        Map.push([r, x0, iterations, logisticMap(r, x0, iterations)])
-    }
-    console.log("Basic Maps done.")
-    Maps.push(Map)
+for (let trial = 1; trial < TRIALS + 1; trial++) {
+    await generateData();
+    console.log(`Trial #${trial} completed.`)
 }
 
-{ // mersenne
-    const x0 = 0.5; // Initial population
-    const iterations = 100; // Number of iterations
-    let Map = []
-    for (var i = 0; i < 15; i++) {
-        Map.push(logisticMap(mersenne(3.5, 4), x0, iterations))
-    }
-    console.log("Mersenne Maps done.")
-    Maps.push(Map)
-}
-
-{ // true
-    const x0 = 0.5; // Initial population
-    const iterations = 100; // Number of iterations
-    let Map = []
-    let Numbers = await trueRandom(3.5, 4, 15)
-    for (let i = 0; i < Numbers.length; i++){
-        let r = Numbers[i]
-        console.log(r)
-        Map.push([r, x0, iterations, logisticMap(r, x0, iterations)])
-    }
-    console.log("True Maps done.")
-    Maps.push(Map)
-}
-
-{ // quantum
-    const x0 = 0.5; // Initial population
-    const iterations = 100; // Number of iterations
-    let Map = []
-    let Numbers = await quantumRandom(3.5, 4, 15)
-    for (let i = 0; i < Numbers.length; i++){
-        let r = Numbers[i]
-        Map.push([r, x0, iterations, logisticMap(r, x0, iterations)])
-    }
-    console.log("Quantum Maps done.")
-    Maps.push(Map)
-}
-
-fs.writeFileSync(`data/${randomUUID()}.txt`, Maps.join("\n"));
+console.log("Finished generating data.")
+console.log(3.5 / 15)
