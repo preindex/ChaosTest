ba79f07f94686f0ddf4999a8a15b7b15c3b56434
preindex <48609126+preindex@users.noreply.github.com>
2024-09-27 00:01:29 -0700

    fixed randomnness of mersenne

--- generators/mersenne.js
+++ generators/mersenne.js
@@ -7,10 +7,14 @@
 const twister = require('mersenne-twister')
 
 export function mersenne(low, high, seed) {
-    var Generator = new twister(seed);
+    var Generator = new twister(process.hrtime());
     var value = Generator.random();
     if (low != null & high != null) {
         value = value * (high - low) + low
     }
     return value;
+}
+
+for (let i = 0; i < 100; i++) {
+    console.log(mersenne(3.5, 4))
 }
