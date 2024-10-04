9b37476fca5aa6b85cc9c08c309e3565636e7ab6
preindex <48609126+preindex@users.noreply.github.com>
2024-10-04 13:56:17 -0700

    add noise to the logistic equation

--- systems/logisticMap.js
+++ systems/logisticMap.js
@@ -1,9 +1,10 @@
-export function logisticMap(r, x0, iterations) { // Actual logistic map function.
+export function logisticMap(r, x0, iterations, noise = 0) { // Actual logistic map function.
+    console.log(noise);
     let x = x0;
     const results = [];
 
     for (let i = 0; i < iterations; i++) {
-        x = r * x * (1 - x);
+        x = r * x * (1 - x) + noise;
         results.push(x);
     }
 
